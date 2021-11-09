6ee4ec32e265620d41095ce629dc09f6816e16ad
Calel <calelbecassi@hotmail.com>
2021-11-08 21:09:44 -0400

    tratando erro de usuário ja cadastrado

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,12 +5,17 @@ const User = require('../models/User');
 const router = express.Router(); 
 
 router.post('/register', async (req, res) => {
+    const { email } = req.body;
+
     try{
+        if(await User.findOne({ email }))
+            return res.send(400).send({ error: 'Usuário já existe' });
+
         const user = await User.create(req.body);
 
         return res.send({ user });
     } catch(err){
-        return res.status(400).send({ error: 'Registration failed' });
+        return res.status(400).send({ error: 'Falha no registro' });
     }
 });
 
