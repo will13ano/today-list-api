8a03239a9db9878fa09d0c1744541ce305f61ee3
Samuel Willian <sw23022001@gmail.com>
2021-11-08 22:34:43 -0400

    adicionado configurações

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ const express = require('express');
 
 const app = express();
 
+// PERMITIR A APLICAÇÃO RECEBER CORPO DE REQUISIÇÃO
+app.use(express.json());
 
-app.listen(3000);
+// PERMITIR A APLICAÇÃO A RECEBER PARAMS
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log('ESCUTANDO...');
+});
