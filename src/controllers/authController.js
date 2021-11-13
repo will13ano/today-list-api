8a41aa8d6046ffd66852686c84d1be9296ad0c9d
thaislarener <thaisla_renner@hotmail.com>
2021-11-13 16:20:38 -0400

    autenticação com token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,6 +7,12 @@ const authConfig = require('../config/auth');
 
 const router = express.Router(); 
 
+function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400,
+    });
+}
+
 router.post('/register', async (req, res) => {
     const { email } = req.body;
 
@@ -19,7 +25,10 @@ router.post('/register', async (req, res) => {
 
         user.password = undefined;
 
-        return res.send({ user });
+        return res.send({ 
+            user,
+            token: generateToken({ id: user.id }),
+        });
     } catch(err){
         return res.status(418).send({ error: 'Falha no registro' });
     }
@@ -40,11 +49,10 @@ router.post('/authenticate', async (req, res) => {
 
     user.password = undefined;
 
-    const token = jwt.sign({ id : user.id }, authConfig.secret, {
-        expiresIn: 86400,
+    res.send({ 
+        user,
+        token: generateToken({ id: user.id }),
     });
-
-    res.send({ user, token });
 });
 
 module.exports = app => app.use('/auth', router);
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 require('./controllers/authController')(app);
 require('./controllers/listController')(app);
 require('./controllers/todoController')(app);
+require('./controllers/projectController')(app);
 
 app.listen(3000, () => {
   console.log('ESCUTANDO...');
